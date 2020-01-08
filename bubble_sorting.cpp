60941cda9e6119e34abca5471ed68cf18940d25b
arensis-julia <julia981028@gmail.com>
2020-01-08 16:47:03 +0900

    bubble sorting format modified

--- bubble_sorting.cpp
+++ bubble_sorting.cpp
@@ -34,11 +34,10 @@ int main() {
     }
 
     // SORTING
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<n-1;j++) {
-            if(v[j] > v[j+1]) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n-1; j++) {
+            if(v[j] > v[j+1])
                 swap(v[j], v[j+1]);
-            }
         }
     }
 
